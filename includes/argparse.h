c1b2eb8322a85232925757ffe0da6b1fbcbf67a5
pmayer <pmayer@cosy.sbg.ac.at>
2021-02-11 11:27:31 +0100

    replaced email adresse

--- includes/argparse.h
+++ includes/argparse.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2021 Philipp Mayer - philmay1992@gmail.com
+ * Copyright 2021 Philipp Mayer - pmayer@cs.sbg.ac.at
  *                Günther Eder  - geder@cs.sbg.ac.at
  *
  * This program is free software: you can redistribute it and/or modify
--- src/polygon.cpp
+++ src/polygon.cpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2021 Philipp Mayer - philmay1992@gmail.com
+ * Copyright 2021 Philipp Mayer - pmayer@cs.sbg.ac.at
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
